7f3d25aa370f7ce24e6e9839b8a73063f9eb5341
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 15:41:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,41 @@
 class Dog
-  # code goes here
+
+
+   attr_reader :name, :species
+   attr_accessor :owner, :mood
+  @@all = []
+  @@count = 0
+  
+  def initialize (name, owner)
+    
+    @name = name
+    @owner = owner
+    @species = "dog"
+    @mood = "nervous"
+    @@all << self
+    @@count +=1
+    
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.reset_all
+    @@all =[]
+    @@count = 0
+  end
+  
+
+
+
+
 end
